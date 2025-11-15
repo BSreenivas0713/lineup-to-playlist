81753330129dcdedeb1397f69eee7abc2a24f376
Bharath Sreenivas <sreenivas.bharath@gmail.com>
2025-11-15 01:49:24 -0500

    update vite config again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173,
   },
   preview: {
-    host: true,
+    host: '0.0.0.0',
     port: process.env.PORT || 5173,
-    strictPort: false
+    strictPort: false,
+    allowedHosts: ['.railway.app', 'localhost']
   }
 })
